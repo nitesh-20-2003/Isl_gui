3d1589297366621de190a426531a50a50dd1b0fd
Nitesh Sharma <niteshsharmauiet@gmail.com>
2024-10-27 19:52:50 +0530

    express server deleted

--- Server/routes/Landmarks.js
+++
@@ -1,5 +0,0 @@
-const express =require('express')
-const router=express.Router();
-const {Landmarks}=require('../controllers/Landmarks');
-router.route('/').post(Landmarks)
-module.exports=router;
--- Server/utils/load_model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import sys
-
-# Load the model from the specified path
-model_path = sys.argv[1]
-model = pickle.load(open('/home/nitesh/Sih/sih/Isl_gui/Server/utils/model.p', 'rb'))
-
-print("Model loaded successfully")
